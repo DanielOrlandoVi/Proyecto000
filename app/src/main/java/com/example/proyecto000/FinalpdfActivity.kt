2235541b2910c8b1bbf08f4d451d799068909a21
Danteeh <boingochavez1@gmail.com>
2023-08-21 20:27:30 -0500

    feat- implementacion primer PDF

---
+++ app/src/main/java/com/example/proyecto000/FinalpdfActivity.kt
@@ -0,0 +1,11 @@
+package com.example.proyecto000
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FinalpdfActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_finalpdf)
+    }
+}
